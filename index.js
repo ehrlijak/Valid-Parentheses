d5d16462234ca17e6b2a0dc4c629e90d649f7cb3
ehrlijak <jakubehrlich6@gmail.com>
2026-03-11 08:22:03 +0100

    Fix for basic test cases

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
-console.log('Happy developing ✨')
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
 
+    let str = s;
+
+    const map = new Map();
+    map.set("(", ')' );
+    map.set("[", ']') ;
+    map.set("{", "}" ) ;
+
+    let temp;
+    let strng = s;
+
+    while (str.length >=1) {
+
+        //temp = str.substr(str.length -1 , str.length)
+        //console.log(temp);
+
+
+        temp = str.substr(0, 1);
+        if (map.has(temp)) {
+            if (map.get(temp) === (str.substr(1, 1))) {
+                strng = str;
+                str = strng.substr(2, strng.length);
+            }
+            else if (map.get(temp) === str.substr(strng.length -1 , strng.length)) {
+                strng = str;
+                str = strng.substr(1, strng.length - 2);
+            }
+            else {
+                return false;
+            }
+
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
+
+    return true;
+};
+
+
+
+console.log(isValid("([{{[]}}])"));
